b5ee21a3826041ee24cf26ee721b8bff12b8867f
Rocketeer817 <71659230+Rocketeer817@users.noreply.github.com>
2023-04-24 17:15:46 +0530

    Time: 174 ms (9.79%), Space: 39.6 MB (6.38%) - LeetHub

--- longest-valid-parentheses/longest-valid-parentheses.cs
+++ longest-valid-parentheses/longest-valid-parentheses.cs
@@ -1,81 +1,41 @@
 public class Solution {
     public int LongestValidParentheses(string s) {
         int n = s.Length;
-
-        int[] dp = new int[n];
-
-        for(int i=0;i<n;i++){
-            dp[i] = -1;
-        }
-        
-        for(int i=n-1;i>=0;i--){
-            generate(s,n,i,dp);
-            Console.WriteLine(dp[i]);
-        }
-
-        int maxLen = 0;
-
-        int ptr = 0;
-
-        int len = 0;
-
-        while(ptr<n){
-            if(dp[ptr]==0){
-                len = 0;
-                ptr++;
-                continue;
-            }
-            len += dp[ptr];
-            maxLen = Math.Max(maxLen,len);
-            ptr += dp[ptr];
-        }
-
-        maxLen = Math.Max(maxLen,len);
-
-        return maxLen;
-    }
-
-
-    public int generate(string A,int n, int idx, int[] dp){
-        if(idx==n){
+        if(n==0){
             return 0;
         }
-        if(dp[idx]!=-1){
-            return dp[idx];
+
+        int[] dp = new int[n+1];
+
+        dp[n] = 0;
+        dp[n-1] = 0;
+
+        int ans = 0;
+
+        Console.WriteLine("start");
+
+        for(int i=n-2;i>=0;i--){
+            Console.WriteLine(i);
+            if(s[i]==')'){
+                dp[i] = 0;
+            }
+            else{
+                int k = dp[i+1];
+                int j = i-1+k+2;
+                if(j<n && s[j]==')'){
+                    dp[i] = k+2 + ((j+1)<n?dp[j+1]:0);
+                }
+                else{
+                    dp[i] = 0;
+                }
+            }
+            ans = Math.Max(ans,dp[i]);
         }
 
-
-        if(A[idx]==')' || idx+1==n){
-            dp[idx] = 0;
-            return dp[idx];
-        }
-        
-        int k = generate(A,n,idx+1,dp);
-        int index = k+idx;
-        if(index+1==n){
-            dp[idx] = 0;
-            return dp[idx];
-        }
-
-        while(index+1<n){
-            if(A[index+1]==')'){
-                dp[idx] = index+1-idx+1;
-                break;
-            }
-            if(k==0){
-                dp[idx]=0;
-                break;
-            }
-            k = generate(A,n,index+1,dp);
-            index = k+index;
-            if(index+1==n){
-                dp[idx]=0;
-            }
-        }
-        
-        
-        return dp[idx];
-        
+        return ans;
     }
+
+
+    
 
 }
