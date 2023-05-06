283db1efcba367ddc32c68ce1d46d9d8e1987f0c
Rocketeer817 <71659230+Rocketeer817@users.noreply.github.com>
2023-05-06 12:31:27 +0530

    Time: 454 ms (57.71%), Space: undefined (undefined) - LeetHub

--- swap-salary/swap-salary.sql
+++ swap-salary/swap-salary.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
 
- update Salary set sex = (case when sex='m' then 'f' else 'm' end);
+#update Salary set sex = (case when sex='m' then 'f' else 'm' end);
+
+update Salary set sex = If(sex='m','f','m');
