ee6f41b44a013fc8086cd9c0b42f586089b58ba6
Rocketeer817 <71659230+Rocketeer817@users.noreply.github.com>
2023-05-08 10:33:09 +0530

    Time: 1233 ms (18.83%), Space: undefined (undefined) - LeetHub

--- rearrange-products-table/rearrange-products-table.sql
+++ rearrange-products-table/rearrange-products-table.sql
@@ -2,15 +2,17 @@
 
 select *
 
-from ( select product_id,"store1" as store,store1 as price from Products where store1 is not null
+from ( select product_id,"store1" as store,store1 as price from Products 
 
         union
 
-        select product_id,"store2" as store,store2 as price from Products where store2 is not null
+        select product_id,"store2" as store,store2 as price from Products 
 
         union
 
-        select product_id,"store3" as store,store3 as price from Products where store3 is not null
+        select product_id,"store3" as store,store3 as price from Products 
 
-) as p ;
+) as p 
+
+where p.price is not null
 
