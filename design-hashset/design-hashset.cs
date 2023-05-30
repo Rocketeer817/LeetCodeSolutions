9262d992a7ae02ab2eee7ee0414d6318faf2956d
Rocketeer817 <71659230+Rocketeer817@users.noreply.github.com>
2023-05-30 15:24:17 +0530

    Time: 253 ms (34.21%), Space: 66.3 MB (72.37%) - LeetHub

--- design-hashset/design-hashset.cs
+++ design-hashset/design-hashset.cs
@@ -5,17 +5,42 @@ public class MyHashSet {
     }
     
     public void Add(int key) {
-        if(!Contains(key)){
-            set.Add(key);
+        int pos = Find(key);
+        if(pos<0 || set[pos]!=key){
+            set.Insert(pos+1,key);
         }
     }
     
     public void Remove(int key) {
-        set.Remove(key);
+        int pos = Find(key);
+        if(pos>=0 && set[pos]==key){
+            set.RemoveAt(pos);
+        }
     }
     
     public bool Contains(int key) {
-        return set.Contains(key);
+        int pos = Find(key);
+        return pos>=0 && set[pos]==key;
+    }
+
+    private int Find(int key){
+        int l = 0;
+        int h = set.Count-1;
+
+        int ans = -1;
+
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(set[mid]<=key){
+                l = mid+1;
+                ans = mid;
+            }
+            else if(set[mid]>key){
+                h = mid-1;
+            }
+        }
+
+        return ans;
     }
 }
 
