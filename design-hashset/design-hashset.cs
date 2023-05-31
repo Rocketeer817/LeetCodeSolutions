6282dfd8e92d5b18db79cdd0b7ee5563bb01c998
Rocketeer817 <71659230+Rocketeer817@users.noreply.github.com>
2023-05-31 09:51:42 +0530

    Time: 249 ms (39.7%), Space: 66.7 MB (44.37%) - LeetHub

--- design-hashset/design-hashset.cs
+++ design-hashset/design-hashset.cs
@@ -1,46 +1,138 @@
 public class MyHashSet {
-    private List<int> set;
+    private Node root;
+
     public MyHashSet() {
-        set = new List<int>();    
+        root = null;
     }
     
     public void Add(int key) {
-        int pos = Find(key);
-        if(pos<0 || set[pos]!=key){
-            set.Insert(pos+1,key);
+        if(root==null){
+            root = new Node(key);
+            return;
         }
+        Insert(root,key);
     }
     
     public void Remove(int key) {
-        int pos = Find(key);
-        if(pos>=0 && set[pos]==key){
-            set.RemoveAt(pos);
-        }
+        Remove(null,root,key,true);
     }
     
     public bool Contains(int key) {
-        int pos = Find(key);
-        return pos>=0 && set[pos]==key;
+        return Find(root,key);
     }
 
-    private int Find(int key){
-        int l = 0;
-        int h = set.Count-1;
-
-        int ans = -1;
-
-        while(l<=h){
-            int mid = l + (h-l)/2;
-            if(set[mid]<=key){
-                l = mid+1;
-                ans = mid;
-            }
-            else if(set[mid]>key){
-                h = mid-1;
-            }
+    public bool Find(Node node, int key){
+        if(node == null){
+            return false;
+        }
+        if(node.val == key){
+            return true;
         }
 
-        return ans;
+        return node.val>key?Find(node.left,key):Find(node.right,key);
+    }
+
+    public void Insert(Node node, int key){
+        if(node.val==key){
+            return;
+        }
+
+        if(node.val>key){
+            if(node.left==null){
+                node.left = new Node(key);
+                return;
+            }
+            else{
+                Insert(node.left,key);
+            }
+        }
+        else{
+            if(node.right==null){
+                node.right = new Node(key);
+                return;
+            }
+            else{
+                Insert(node.right,key);
+            }
+        }
+    }
+
+    public void Remove(Node parent,Node node, int key,bool left){
+        if(node==null){
+            return;
+        }
+        if(node.val == key){
+            
+            if(node.left == null && node.right == null){
+                if(parent==null){
+                    root = null;
+                }
+                else{
+                    RemoveNode(parent,node,left);
+                }               
+            }
+
+            int x = FindInorderSuccessor(node.right);
+
+            if(x==-1){
+                if(parent==null){
+                    root = node.left;
+                }
+                else if(left){                   
+                    parent.left = node.left;                                       
+                }
+                else{
+                    parent.right = node.left;
+                }
+            }
+            else{
+                node.val = x;
+                Remove(node,node.right,x,false);
+            }
+            
+            return;
+        }
+
+        if(node.val<key){
+            Remove(node,node.right,key,false);
+        }
+        else{
+            Remove(node,node.left,key,true);
+        }
+
+
+    }
+
+    public void RemoveNode(Node parent,Node node,bool left){
+        if(left){
+            parent.left = null;
+        }
+        else{
+            parent.right = null;
+        }
+    }
+
+    public int FindInorderSuccessor(Node node){
+        if(node==null){
+            return -1;
+        }
+
+        if(node.left == null){
+            return node.val;
+        }
+
+        return FindInorderSuccessor(node.left);
+    }
+
+}
+
+public class Node{
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node(int x){
+        val = x;
     }
 }
 
